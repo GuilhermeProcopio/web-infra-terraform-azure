e285d2e8574e8dbc7961fc72172f5ddde2d3b6a6
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-07-23 20:50:26 -0300

    feat: adding subnet and NIC for VMs

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -12,22 +12,21 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "acme_subnet" {
-  name = "acme-subnet"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                 = "acme-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = ["10.0.0.0/24"]
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_network_interface" "acme-nic" {
-    count = 2
-    name = "acme-vm-nic-${count.index}"
-    location = var.resource_group_location
-    resource_group_name = var.resource_group_name_prefix
+  count               = 2
+  name                = "acme-vm-nic-${count.index}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name_prefix
 
-    ip_configuration {
-      name = "nic-${count.index}"
-      subnet_id = azurerm_subnet.acme_subnet.id
-      private_ip_address_allocation = "Dynamic"
-    }
-
+  ip_configuration {
+    name                          = "nic-${count.index}"
+    subnet_id                     = azurerm_subnet.acme_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
