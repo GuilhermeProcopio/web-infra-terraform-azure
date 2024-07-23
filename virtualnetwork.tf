5700f562251aae161db16f3b1d0fbd6180f13d2a
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-07-23 20:59:45 -0300

    feat: adding public ip

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -4,6 +4,13 @@ resource "azurerm_network_security_group" "vnet-security-group" {
   resource_group_name = azurerm_resource_group.example.name
 }
 
+resource "azurerm_public_ip" "public-ip" {
+  name                = "acme-public-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "acme-vnet"
   location            = var.resource_group_location
