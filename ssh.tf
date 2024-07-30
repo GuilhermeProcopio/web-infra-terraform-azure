f1741ed247baa340497b208d12888022177ea096
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-07-30 14:32:00 -0300

    feat: upping to date

--- ssh.tf
+++ ssh.tf
@@ -21,6 +21,6 @@ resource "azapi_resource" "ssh_public_key" {
 
 
 resource "local_file" "private_key" {
-  content  = azapi_resource_action.ssh_public_key_gen.output.private_key
+  content  = azapi_resource_action.ssh_public_key_gen.output.privateKey
   filename = "private_key.pem"
 }
--- variables.tf
+++ variables.tf
@@ -8,3 +8,9 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix to combinate with a randomly name created"
 }
+
+variable "vm_user" {
+  type        = string
+  default     = "vm-user"
+  description = "User to auth on the VM"
+}
